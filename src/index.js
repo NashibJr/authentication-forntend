38376f12eb4ddbd6c2346738e0fe327943b3fa84
NashibJr <nashibjr@gmail.com>
2023-05-05 00:42:01 -0700

    removing the styles folder coz its not necessary

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/users/store";
-import "./styles/home.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
